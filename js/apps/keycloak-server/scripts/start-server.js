bed5cfe458dd8e8e6525aa3ca4862538f4966b6d
Nikola Brodic <n.brodic@vegait.rs>
2024-12-24 11:40:03 +0100

    Set version to 24.0.5 in remaining files

--- js/apps/keycloak-server/scripts/start-server.js
+++ js/apps/keycloak-server/scripts/start-server.js
@@ -14,7 +14,7 @@ import { parseArgs } from "node:util";
 const DIR_NAME = path.dirname(fileURLToPath(import.meta.url));
 const SERVER_DIR = path.resolve(DIR_NAME, "../server");
 const LOCAL_QUARKUS = path.resolve(DIR_NAME, "../../../../quarkus/dist/target");
-const LOCAL_DIST_NAME = "keycloak-999.0.0-SNAPSHOT.tar.gz";
+const LOCAL_DIST_NAME = "keycloak-24.0.5.tar.gz";
 const SCRIPT_EXTENSION = process.platform === "win32" ? ".bat" : ".sh";
 const ADMIN_USERNAME = "admin";
 const ADMIN_PASSWORD = "admin";
@@ -140,7 +140,7 @@ async function getNightlyAsset() {
   });
 
   return release.data.assets.find(
-    ({ name }) => name === "keycloak-999.0.0-SNAPSHOT.tar.gz",
+    ({ name }) => name === "keycloak-24.0.5.tar.gz",
   );
 }
 
