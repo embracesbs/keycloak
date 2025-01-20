764f06e66be1b3b31ac7217a33b5756ddb6b0c29
Nikola Brodic <n.brodic@vegait.rs>
2025-01-20 10:48:37 +0100

    Cherry-pick 'Embrace Multitenancy support' commit from 26.1.0 to 26.2.3

--- server-spi-private/src/main/java/org/keycloak/models/utils/ModelToRepresentation.java
+++ server-spi-private/src/main/java/org/keycloak/models/utils/ModelToRepresentation.java
@@ -802,6 +802,7 @@ public class ModelToRepresentation {
 
             if (resourceServer != null) {
                 rep.setAuthorizationServicesEnabled(true);
+                rep.setAuthorizationSettings(toRepresentation(resourceServer, clientModel));
             }
         }
 
@@ -1092,9 +1093,12 @@ public class ModelToRepresentation {
         representation.setLogic(policy.getLogic());
 
         if (allFields) {
+
+            //Embrace modification, remove deep copy
             representation.setResourcesData(policy.getResources().stream()
-                    .map(resource -> toRepresentation(resource, policy.getResourceServer(), authorization, true))
+                    .map(resource -> toRepresentation(resource, policy.getResourceServer(), authorization))
                     .collect(Collectors.toSet()));
+
             representation.setScopesData(policy.getScopes().stream().map(
                     resource -> toRepresentation(resource)).collect(Collectors.toSet()));
         }
